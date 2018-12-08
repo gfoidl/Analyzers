ac7fba64248a90293c590792a632651fff0b2686
Günther Foidl <gue@korporal.at>
2018-12-08 16:10:08 +0100

    PubternalityAnalyzer added

---
+++ source/gfoidl.Analyzers/PubternalityAnalyzer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+// Based on https://github.com/aspnet/Extensions/pull/345
+
+namespace gfoidl.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PubternalityAnalyzer : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+        //---------------------------------------------------------------------
+        public PubternalityAnalyzer()
+        {
+            this.SupportedDiagnostics = ImmutableArray.Create(new[]
+            {
+                PubturnalityDescriptors.GF0001
+            });
+        }
+        //---------------------------------------------------------------------
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            context.RegisterCompilationStartAction(analysisContext =>
+            {
+                analysisContext.RegisterSyntaxNodeAction(AnalyzeTypeUsage, SyntaxKind.IdentifierName);
+            });
+        }
+        //---------------------------------------------------------------------
+        private static void AnalyzeTypeUsage(SyntaxNodeAnalysisContext syntaxContext)
+        {
+            IdentifierNameSyntax identifier = syntaxContext.Node as IdentifierNameSyntax;
+
+            TypeInfo symbolInfo = ModelExtensions.GetTypeInfo(syntaxContext.SemanticModel, identifier, syntaxContext.CancellationToken);
+
+            if (symbolInfo.Type == null)
+                return;
+
+            ITypeSymbol type = symbolInfo.Type;
+
+            if (!IsInternal(type.ContainingNamespace))
+            {
+                // don't care about non-pubternal type references
+                return;
+            }
+
+            if (!syntaxContext.ContainingSymbol.ContainingAssembly.Equals(type.ContainingAssembly))
+                syntaxContext.ReportDiagnostic(Diagnostic.Create(
+                    PubturnalityDescriptors.GF0001,
+                    identifier.GetLocation(),
+                    type.ToDisplayString()));
+        }
+        //---------------------------------------------------------------------
+        private static bool IsInternal(INamespaceSymbol ns)
+        {
+            while (ns != null)
+            {
+                if (ns.Name == "Internal")
+                    return true;
+
+                ns = ns.ContainingNamespace;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ source/gfoidl.Analyzers/PubturnalityDescriptors.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace gfoidl.Analyzers
+{
+    internal static class PubturnalityDescriptors
+    {
+        public static readonly DiagnosticDescriptor GF0001 = new DiagnosticDescriptor(
+            "GF0001",
+            "Cross assembly pubternal reference",
+            "Cross assembly pubternal type ('{0}') reference. This type may be changed to 'internal', so don't use it.",
+            "Usage",
+            DiagnosticSeverity.Warning, true);
+    }
+}
